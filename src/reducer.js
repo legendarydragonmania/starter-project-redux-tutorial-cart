ddf22833aa25752fc48b8012ca1b1071796d63da
Yaqubboy <yaqubboyyaqubboyev45@gmail.com>
2022-05-16 12:28:12 +0500

    fixing actions

--- src/reducer.js
+++ src/reducer.js
@@ -1,4 +1,11 @@
-import { CLEAR_CART, DECREASE, INCREASE } from './actions'
+import {
+  CLEAR_CART,
+  DECREASE,
+  INCREASE,
+  REMOVE,
+  GET_AMOUNT,
+  GET_TOTAL,
+} from './actions'
 import cartItems from './cart-items'
 
 export const initialStore = {
@@ -8,17 +15,51 @@ export const initialStore = {
 }
 
 function reducer(state, action) {
-  switch (action.type) {
-    case CLEAR_CART:
-      return {
-        ...state,
-        cart: [],
-        total: 0,
-        amount: 0,
-      }
-    default:
-      return state
+  if (action.type === CLEAR_CART) {
+    return {
+      ...state,
+      cart: [],
+      total: 0,
+      amount: 0,
+    }
   }
+  if (action.type === REMOVE) {
+    return {
+      ...state,
+      cart: state.cart.filter((item) => item.id !== action.payload.id),
+    }
+  }
+  if (action.type === INCREASE) {
+    let tempCart = state.cart.map((item) => {
+      if (item.id === action.payload.id) {
+        item = { ...item, amount: item.amount + 1 }
+      }
+      return item
+    })
+
+    return {
+      ...state,
+      cart: tempCart,
+    }
+  }
+  if (action.type === DECREASE) {
+    let tempCart = []
+    if (action.payload.amount === 1) {
+      tempCart = state.cart.filter((item) => item.id !== action.payload.id)
+    } else {
+      tempCart = state.cart.map((item) => {
+        if (item.id === action.payload.id) {
+          item = { ...item, amount: item.amount - 1 }
+        }
+        return item
+      })
+    }
+    return {
+      ...state,
+      cart: tempCart,
+    }
+  }
+  return state
 }
 
 export default reducer
